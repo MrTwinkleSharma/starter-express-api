fd33ce20d5605f8aa264b9de270297b2414133eb
Mr. Twinkle Sharma <smtwinkle451@gmail.com>
2023-01-25 17:41:46 +0530

    Generalized

--- index.js
+++ index.js
@@ -14,19 +14,20 @@ const app = express();
 
 app.use(express.json()); // to accept json data
 
-// For CORS Errors
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE', 'OPTIONS');
+// // For CORS Errors
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+//   );
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE', 'OPTIONS');
 
-  next();
-});
+//   next();
+// });
 
 app.options('*', cors());
+app.use(cors());
 
 // app.use(cors({
 //   origin:"https://chat-nexus.netlify.app",
