a45c37ff2ae880316ad9d76fd4e7c7e1af18173f
Mr. Twinkle Sharma <smtwinkle451@gmail.com>
2023-01-25 19:07:03 +0530

    socket removed

--- index.js
+++ index.js
@@ -70,57 +70,57 @@ const server = app.listen(
   console.log(`Server running on PORT ${PORT}...`.yellow.bold)
 );
 
-const io = new Server(server, {
-  // const io = require("socket.io")(server, {
-  pingTimeout: 60000,
-  cors: {
-    origin: ["*", "http://localhost:3000", 'https://chat-nexus.netlify.app/chats', 'https://chat-nexus.netlify.app'],
-  },
-  handlePreflightRequest: (req, res) => {
-    res.writeHead(200, {
-      "Access-Control-Allow-Origin":"*",
-      "Access-Control-Allow-Methods":"GET, POST",
-      "Access-Control-Allow-Headers":"my-custom-header",
-      "Access-Control-Allow-Credentials":true
-    });
-    res.end();
-  }
-});
+// const io = new Server(server, {
+//   // const io = require("socket.io")(server, {
+//   pingTimeout: 60000,
+//   cors: {
+//     origin: ["*", "http://localhost:3000", 'https://chat-nexus.netlify.app/chats', 'https://chat-nexus.netlify.app'],
+//   },
+//   handlePreflightRequest: (req, res) => {
+//     res.writeHead(200, {
+//       "Access-Control-Allow-Origin":"*",
+//       "Access-Control-Allow-Methods":"GET, POST",
+//       "Access-Control-Allow-Headers":"my-custom-header",
+//       "Access-Control-Allow-Credentials":true
+//     });
+//     res.end();
+//   }
+// });
 
-io.on("connection", (socket) => {
-  socket.on('error', function (err) {
-    console.log("Socket.IO Error");
-    console.log(err.stack); // this is changed from your code in last comment
-  });
+// io.on("connection", (socket) => {
+//   socket.on('error', function (err) {
+//     console.log("Socket.IO Error");
+//     console.log(err.stack); // this is changed from your code in last comment
+//   });
 
-  console.log("Connected to socket.io");
-  socket.on("setup", (userData) => {
-    console.log(userData);
-    socket.join(userData?._id);
-    socket.emit("connected");
-  });
+//   console.log("Connected to socket.io");
+//   socket.on("setup", (userData) => {
+//     console.log(userData);
+//     socket.join(userData?._id);
+//     socket.emit("connected");
+//   });
 
-  socket.on("join chat", (room) => {
-    socket.join(room);
-    console.log("User Joined Room: " + room);
-  });
-  socket.on("typing", (room) => socket.in(room).emit("typing"));
-  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+//   socket.on("join chat", (room) => {
+//     socket.join(room);
+//     console.log("User Joined Room: " + room);
+//   });
+//   socket.on("typing", (room) => socket.in(room).emit("typing"));
+//   socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
 
-  socket.on("new message", (newMessageRecieved) => {
-    var chat = newMessageRecieved.chat;
+//   socket.on("new message", (newMessageRecieved) => {
+//     var chat = newMessageRecieved.chat;
 
-    if (!chat.users) return console.log("chat.users not defined");
+//     if (!chat.users) return console.log("chat.users not defined");
 
-    chat.users.forEach((user) => {
-      if (user._id == newMessageRecieved.sender._id) return;
+//     chat.users.forEach((user) => {
+//       if (user._id == newMessageRecieved.sender._id) return;
 
-      socket.in(user._id).emit("message recieved", newMessageRecieved);
-    });
-  });
+//       socket.in(user._id).emit("message recieved", newMessageRecieved);
+//     });
+//   });
 
-  socket.off("setup", () => {
-    console.log("USER DISCONNECTED");
-    socket.leave(userData._id);
-  });
-});
+//   socket.off("setup", () => {
+//     console.log("USER DISCONNECTED");
+//     socket.leave(userData._id);
+//   });
+// });
